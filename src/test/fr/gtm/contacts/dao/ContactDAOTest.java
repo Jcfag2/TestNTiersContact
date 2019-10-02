bb20b8bed7ab5a9651618e539a205ba438636184
jcfag2 <j.fagothey@esff.fr>
2019-10-02 17:03:17 +0200

    premier push

---
+++ src/test/fr/gtm/contacts/dao/ContactDAOTest.java
@@ -0,0 +1,69 @@
+package fr.gtm.contacts.dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import fr.gtm.contacts.entities.Contact;
+
+public class ContactDAOTest {
+	private Contact[] contacts = {
+			new Contact("M","Lagaffe","Gaston"),
+			new Contact("Mme","Blanc-Sec","Adèle"),
+			new Contact("M","Leblanc","Juste"),
+			new Contact("M","Brown","Emmet"),
+			new Contact("M","Gordon","James"),
+			new Contact("Mme","Croft","Lara")
+	};
+	private ContactDAO dao = new ContactMockDAO();
+	
+	@Test
+	public void testSave() {
+		Contact c = contacts[0];
+		dao.save(c);
+		int nb = dao.getAllContacts().size();
+		assertEquals(1, nb);
+		assertNotEquals(0, c.getId());
+	}
+
+	@Test
+	public void testUpdate() {
+		Contact c = contacts[0];
+		dao.save(c);
+		c.setNom("foo");
+		dao.update(c);
+		Contact c1 = dao.getContactById(c.getId());
+		assertEquals("foo", c1.getNom());
+	}
+
+	@Test
+	public void testDelete() {
+		
+	}
+
+
+	@Test
+	public void testGetAllContacts() {
+		
+	}
+
+
+	@Test
+	public void testGetContactById() {
+		
+	}
+
+
+	@Test
+	public void testGetContactsByNom() {
+		
+	}
+
+
+	@Test
+	public void testGetContactsByCivilite() {
+		
+	}
+
+
+}
